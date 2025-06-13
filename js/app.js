cb216c54f423dff69cc8d16dd7e6d8ecf2722e32
SegaGLm <sega.glm@gmail.com>
2025-06-13 15:38:43 +0300

    Initial commit of new agency website

---
+++ js/app.js
@@ -0,0 +1,15 @@
+VANTA.WAVES({
+  el: "#intro",
+  mouseControls: true,
+  touchControls: true,
+  gyroControls: false,
+  minHeight: 200.0,
+  minWidth: 200.0,
+  scale: 1.0,
+  scaleMobile: 1.0,
+  color: 0x576a48,
+  shininess: 33.0,
+  waveHeight: 17.5,
+  waveSpeed: 0.65,
+  zoom: 1.05,
+});
